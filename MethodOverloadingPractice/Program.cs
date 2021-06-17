2bdb9b8ee6035837e6bb087ca1bc045559692057
Whit Stroup <email@example.com>
2021-06-17 13:42:41 -0500

    yes

---
+++ MethodOverloadingPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MethodOverloadingPractice
+{
+    class Program
+    {
+        //TODO Create a method that takes two string names in the parameter. Have the method tell you which name is longer.
+        //TODO Create an overload method that takes a list of string names and an integer. The method will look for the first name that matches the length of the integer.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //TODO Call both methods here. Use a break point and watch as the code jumps to the different method overloads.
+
+        }
+
+     
+    }
+}
